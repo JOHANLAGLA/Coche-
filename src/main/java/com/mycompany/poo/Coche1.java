6d4f79d2d39455439ae3253d1dbe94d20babed78
HP <HP@192.168.0.108>
2023-12-22 03:27:45 -0500

    Tarea inicial

---
+++ src/main/java/com/mycompany/poo/Coche1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo;
+
+/**
+ *
+ * @author HP
+ */
+public class Coche1 {
+    //atributos 
+    String color;
+    String marca;
+    int km;
+}
---
+++ src/main/java/com/mycompany/poo/POO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo;
+
+/**
+ *
+ * @author HP
+ */
+public class POO {
+
+    public static void main(String[] args) {
+        Coche1 coche2 = new Coche1();
+        
+        coche2.color = "Negro";
+        coche2.marca = "Toyota";
+        coche2.km = 10;
+        
+        System.out.println("El color  es: "+coche2.color);
+        System.out.println("La marca es :"+coche2.marca);
+        System.out.println("El km es :"+coche2.km);
+    }
+}
